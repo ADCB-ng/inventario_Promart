9ccf92362d00a022fa4bea77f77370b453b35cdf
ADCB-ng <angelblacido1@gmail.com>
2026-06-29 23:49:49 -0500

    ajustes con el los campos, que aparezcan luego de loggear

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,105 +4,146 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Inventario Promart</title>
 
-    <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-    <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
+
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand"  href="{{ route('home') }}">
-                   Promart
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+<div id="app">
 
-                 <ul class="navbar-nav me-auto">
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div class="container">
+
+        <!-- Logo -->
+        <a class="navbar-brand" href="{{ route('home') }}">
+            Promart
+        </a>
+
+        <button class="navbar-toggler" type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+            <!-- Menú izquierdo -->
+            <ul class="navbar-nav me-auto">
+
+                @auth
+
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/empleados') }}">
+                        Empleados
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/productos') }}">
+                        Productos
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/proveedores') }}">
+                        Proveedores
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/areas') }}">
+                        Áreas
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/estados') }}">
+                        Estados
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/pedido-proveedores') }}">
+                        Pedidos
+                    </a>
+                </li>
+
+                @endauth
+
+            </ul>
+
+            <!-- Menú derecho -->
+            <ul class="navbar-nav ms-auto">
+
+                @guest
 
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/empleados') }}">Empleados</a>
+                        <a class="nav-link" href="{{ route('login') }}">
+                            Iniciar sesión
+                        </a>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/productos') }}">Productos</a>
+                        <a class="nav-link" href="{{ route('register') }}">
+                            Registrarse
+                        </a>
                     </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/proveedores') }}">Proveedores</a>
+                @else
+
+                    <li class="nav-item dropdown">
+
+                        <a id="navbarDropdown"
+                           class="nav-link dropdown-toggle"
+                           href="#"
+                           role="button"
+                           data-bs-toggle="dropdown">
+
+                            {{ Auth::user()->name }}
+
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-end">
+
+                            <a class="dropdown-item"
+                               href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                               document.getElementById('logout-form').submit();">
+
+                                Cerrar sesión
+
+                            </a>
+
+                            <form id="logout-form"
+                                  action="{{ route('logout') }}"
+                                  method="POST"
+                                  class="d-none">
+                                @csrf
+                            </form>
+
+                        </div>
+
                     </li>
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/areas') }}">Áreas</a>
-                    </li>
+                @endguest
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/estados') }}">Estados</a>
-                    </li>
+            </ul>
 
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ url('/pedido-proveedores') }}">Pedidos</a>
-                    </li>
+        </div>
 
-                </ul>
-  
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Iniciar sesión') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrarse') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Cerrar sesión') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
     </div>
+</nav>
+
+<main class="py-4">
+    @yield('content')
+</main>
+
+</div>
 </body>
 </html>
